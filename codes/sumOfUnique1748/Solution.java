7a5a5967456ab58266e9c2ee605fde528b23f4fd
AipingRoll <1196761828@qq.com>
2022-02-06 20:32:30 +0800

    给你一个整数数组 nums 。数组中唯一元素是那些只出现 恰好一次 的元素。
    请你返回 nums 中唯一元素的 和 。(哈希的思想)

---
+++ codes/sumOfUnique1748/Solution.java
@@ -0,0 +1,57 @@
+package codes.DayDayUp.codes.sumOfUnique1748;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	
+	private int[] data = {2,3,1,3,4,5};
+	private int ans;
+
+	public Solution() {
+		// TODO Auto-generated constructor stub
+		this.ans = 0;
+	}
+	
+	private int hashSolution(int[] nums) {
+		
+		Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+		int ans = 0;
+		for(int i : nums){
+			counts.put(i, counts.getOrDefault(i, 0) + 1);
+		}
+		
+		for(Map.Entry<Integer, Integer> entry : counts.entrySet()){
+			if(entry.getValue() == 1){
+				ans += entry.getKey();
+			}
+		}
+//		System.out.println(ans);
+		return ans;
+	}
+	
+	//桶排序原理，（最简单的hash）
+	private int ButtSoltion(int[] nums) {
+		int[] counts = new int[100];//方法只能解决[0-100]的整数
+		int ans = 0;
+		//入桶
+		for(int num : nums){
+			counts[num-1]++;
+		}
+		for(int i=0; i<counts.length; i++){
+			if(counts[i] == 1){
+				ans = ans + i + 1;
+			}
+		}
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution solution = new Solution();
+		System.out.println(solution.ButtSoltion(solution.data));
+		
+
+	}
+
+}
