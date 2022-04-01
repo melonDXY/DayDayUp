5851ea6bf787b7cffcd4a4652bd9c3176faa8a60
clownindark@163.com <clownindark@163.com>
2022-04-01 19:49:55 +0800

    找到链表倒数第k个节点(快慢指针滑动)

---
+++ codes/GetKthFromEnd22/Solution.java
@@ -0,0 +1,40 @@
+package codes.GetKthFromEnd22;
+
+/*
+ * public class ListNode {
+ *   int val;
+ *   ListNode next = null;
+ *   public ListNode(int val) {
+ *     this.val = val;
+ *   }
+ * }
+ */
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param pHead ListNode类 
+     * @param k int整型 
+     * @return ListNode类
+     */
+    public ListNode FindKthToTail (ListNode pHead, int k) {
+        // write code here
+        ListNode point = null;
+        if (pHead == null || k == 0) return point;
+        point = pHead;
+        int window = 1;
+        while(point != null) {
+            while (window < k && point != null) {
+                point = point.next;
+                window++;
+            }
+            if (point == null) return point;
+            if (point.next == null) return pHead;
+            point = point.next;
+            pHead = pHead.next;
+        }
+        return pHead;
+    }
+}
