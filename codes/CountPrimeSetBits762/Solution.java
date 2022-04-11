b93ebd08a660109a4ae46b283a4174d6062ae980
clownindark@163.com <clownindark@163.com>
2022-04-11 21:35:15 +0800

    二进制表示中质数个计算置位

---
+++ codes/CountPrimeSetBits762/Solution.java
@@ -0,0 +1,40 @@
+package codes.CountPrimeSetBits762;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    
+    public int countPrimeSetBits(int left, int right) {
+        int ans = 0;
+        List<Integer> primeList = new ArrayList<>();
+        for (int i = 2; i < 21; i++) {
+            Boolean isPrime = true;
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) primeList.add(i);
+            isPrime = true;
+        }
+        
+        for (int i = left ; i<=right; i++) {
+            int count = Integer.bitCount(i);
+            
+            int tmp = i;
+            while ( tmp != 0) {
+                tmp = tmp & (tmp - 1);
+                count++;
+            }
+            if (primeList.contains(count)) ans++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Double double1=Math.pow(10, 6);
+        System.out.println(Integer.toBinaryString(double1.intValue()));
+    }
+}
