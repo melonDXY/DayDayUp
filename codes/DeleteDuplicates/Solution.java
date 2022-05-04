a62f24d9c767139dd43f041283b27d8f7eaf42da
clownindark@163.com <clownindark@163.com>
2022-05-04 23:00:17 +0800

    删除链表中重复的元素

---
+++ codes/DeleteDuplicates/Solution.java
@@ -0,0 +1,46 @@
+package codes.DeleteDuplicates;
+
+import java.util.*;
+
+/*
+ * public class ListNode {
+ *   int val;
+ *   ListNode next = null;
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next = null;
+    public ListNode(int val) {
+      this.val = val;
+    }
+}
+
+public class Solution {
+    /**
+     * 
+     * @param head ListNode类 
+     * @return ListNode类
+     */
+    public ListNode deleteDuplicates (ListNode head) {
+        // write code here
+        if (head == null || head.next == null)
+            return head;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        
+        while (cur != null){
+            if (pre.val == cur.val){
+                pre.next = cur.next;
+                cur = cur.next;
+            }
+            if (cur == null){
+                return head;
+            }
+            pre = pre.next;
+            cur = cur.next;
+        }
+
+        return head;
+    }
+}
